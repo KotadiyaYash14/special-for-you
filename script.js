f19b3239659674e01486b65e8a7418ea2d88c688
Yash Kotadiya <yash.k@shaligraminfotech.com>
2026-06-01 11:57:47 +0530

    change in all three files

--- script.js
+++ script.js
@@ -1,30 +1,62 @@
 const messages = [
-    ""
+    "From the moment you came into my life, everything became more beautiful ❤️",
+
+    "Your smile is my favorite view in the whole world ✨",
+
+    "You are the reason my heart feels so alive 💖",
+
+    "Every day with you feels like a dream 🌹",
+
+    "I don't need perfect moments, I just need you ❤️",
+
+    "Thank you for being the most beautiful chapter of my life 💕"
 ];
 
-let index = 0;
+function showLoveMessage() {
 
-function changeMessage() {
-    document.getElementById("message").innerText =
-        messages[index];
+    const messageBox =
+        document.getElementById("message");
 
-    index++;
+    const randomMessage =
+        messages[
+            Math.floor(
+                Math.random() * messages.length
+            )
+        ];
 
-    if(index >= messages.length){
-        index = 0;
-    }
+    messageBox.innerHTML = "";
+
+    let i = 0;
+
+    const typing =
+        setInterval(() => {
+
+            messageBox.innerHTML +=
+                randomMessage.charAt(i);
+
+            i++;
+
+            if (i >= randomMessage.length) {
+                clearInterval(typing);
+            }
+
+        }, 40);
 }
 
-changeMessage();
-setInterval(changeMessage, 3000);
-
 function createHeart() {
-    const heart = document.createElement("div");
+
+    const heart =
+        document.createElement("div");
 
     heart.classList.add("heart");
-    heart.innerHTML = "";
 
-    heart.style.left = Math.random() * 100 + "vw";
+    heart.innerHTML = "❤️";
+
+    heart.style.left =
+        Math.random() * 100 + "vw";
+
+    heart.style.fontSize =
+        Math.random() * 20 + 20 + "px";
 
     document.body.appendChild(heart);
 
@@ -33,4 +65,33 @@ function createHeart() {
     }, 8000);
 }
 
-setInterval(createHeart, 400);
+setInterval(createHeart, 300);
+
+function createBalloon() {
+
+    const balloon =
+        document.createElement("div");
+
+    balloon.classList.add("balloon");
+
+    const balloons =
+        ["🎈", "💖", "💕"];
+
+    balloon.innerHTML =
+        balloons[
+            Math.floor(
+                Math.random() * balloons.length
+            )
+        ];
+
+    balloon.style.left =
+        Math.random() * 100 + "vw";
+
+    document.body.appendChild(balloon);
+
+    setTimeout(() => {
+        balloon.remove();
+    }, 12000);
+}
+
+setInterval(createBalloon, 2000);
